032177d089e6bcdbc5586e998ea65cffca7bc3ec
Minh Quan <quanhugo2505@gmail.com>
2026-01-06 23:46:27 +0700

    update setup.ps1

--- server/setup.ps1
+++ server/setup.ps1
@@ -1,25 +1,16 @@
 # Stop on error
 $ErrorActionPreference = "Stop"
 
-$MODEL_BASE = "qwen2.5:3b"
-$DECOMPOSER_NAME = "claim-splitter"
-$NORMALIZER_NAME = "evidence-normalizer"
-$VERIFIER_NAME = "fact-checker"
-
 Write-Host "Installing Python dependencies..."
 pip install -r requirements.txt
 
-Write-Host "Pulling base model..."
-ollama pull $MODEL_BASE
+Write-Host "Running seeding..."
+python data/seeding.py
 
-Write-Host "Creating Decomposer model..."
-ollama create $DECOMPOSER_NAME -f ./prompt/decomposer.prompt
+Write-Host "Running embedding..."
+python data/embedding.py
 
-Write-Host "Creating Normalizer model..."
-ollama create $NORMALIZER_NAME -f ./prompt/normalizer.prompt
+Write-Host "Running retrieving for testing..."
+python data/retrieving.py
 
-Write-Host "Creating Verifier model..."
-ollama create $VERIFIER_NAME -f ./prompt/verifier.prompt
-
-Write-Host "Running application..."
-python main.py
+Write-Host "Setup completed."
