e9289c3e4fabed0a2eb4fb86fb521b3dc836bbd3
Minh Quan <quanhugo2505@gmail.com>
2026-01-06 23:51:26 +0700

    Update directory for vectordb

--- server/setup.ps1
+++ server/setup.ps1
@@ -7,8 +7,8 @@ pip install -r requirements.txt
 Write-Host "Running seeding..."
 python data/seeding.py
 
-Write-Host "Running embedding..."
-python data/embedding.py
+# Write-Host "Running embedding..."
+# python data/embedding.py
 
 Write-Host "Running retrieving for testing..."
 python data/retrieving.py
